658712e21805a9e346796a2d8ea187ea38dbaa92
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-02 11:44:08 +1300

    tutorial 10 complete

---
+++ Tutorial 10/Tutorial 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 1000);
+            int[] newArray = new int[5];
+
+            for (int i = 0; i <= 4; i++ )
+            {
+                number = rnd.Next(1, 1000);
+                newArray[i] = number;
+                Console.WriteLine($"Random Number {newArray[i]}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
