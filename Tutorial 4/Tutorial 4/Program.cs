2ddf6d966d3beefcdd0f2e8bbac5ea366c0903bc
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 14:12:47 +1300

    Tutorial 4 complete

---
+++ Tutorial 4/Tutorial 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 1;
+            var number2 = 2;
+
+            Console.WriteLine("FirstNumber:");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("SecondNumber");
+            number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your first number is: {0}", number1);
+            Console.WriteLine("Your second number is: {0}", number2);
+
+            var output = number1 * number2;
+            Console.WriteLine($"{number1} x {number2} = {output}");
+
+            Console.ReadLine();
+        }
+    }
+}
