59ba05fbd76f34d4598ebde2ce189b3c374b93c7
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 14:05:11 +1300

    Tutorial 3 Complete

---
+++ Tutorial 3/Tutorial 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 6;
+            var number2 = 3;
+
+            Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+            Console.ReadLine();
+        }
+    }
+}
