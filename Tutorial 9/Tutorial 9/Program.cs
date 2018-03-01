f1b8d28bfa6192bfeb904c9c289857e1d97b3ea2
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 19:52:45 +1300

    Tutorial 9 Complete

---
+++ Tutorial 9/Tutorial 9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] newArray = new int[3];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                var choices = i + 1;
+                Console.WriteLine($"Enter number {choices} and press enter:  ");
+                newArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(newArray);
+            int lowestNumber = newArray[0];
+            int middleNumber = newArray[1];
+            int highestNumber = newArray[2];
+
+                Console.WriteLine($"Highest number is {highestNumber}");
+                Console.WriteLine($"Middle number is {middleNumber}");
+                Console.WriteLine($"Lowest number is {lowestNumber}");
+            
+        }
+    }
+}
