6cfd3eb29ee969d1e0ef747d4f3c48f7b61af1cc
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 15:20:12 +1300

    Tutorial 5 complete

--- Tutorial 5/Tutorial 5/Program.cs
+++ Tutorial 5/Tutorial 5/Program.cs
@@ -10,6 +10,16 @@ namespace Tutorial_5
     {
         static void Main(string[] args)
         {
+            var input = 15;
+            Console.WriteLine("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<= 10; i++)
+            {
+                Console.WriteLine($"your number {input} = {i * input}");    
+
+            }
+            Console.ReadLine();
         }
     }
 }
