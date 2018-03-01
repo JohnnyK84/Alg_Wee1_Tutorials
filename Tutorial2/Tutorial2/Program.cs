cd0de246a2ae66b4ff1e890df432c53ba3774efa
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 14:00:11 +1300

    Tutorial 2 Complete

---
+++ Tutorial2/Tutorial2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 12;
+            var number2 = 11;
+            var sum = number1 + number2;
+
+            Console.WriteLine($"{number1} + {number2} =: {sum}");
+        }
+    }
+}
