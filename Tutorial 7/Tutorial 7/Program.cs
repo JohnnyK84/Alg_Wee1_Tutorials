2e79b97e7b912f719c90488f946de6e7a4a483be
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 19:20:25 +1300

    tutorial 7 / condition statement compolete

---
+++ Tutorial 7/Tutorial 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // example of swapping two variables
+
+            var a = 10;
+            var b = 20;
+            Console.WriteLine($"var a = {a} var b = {b}");
+
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            Console.WriteLine($"var a = {a} var b = {b}"); // variables swapped using xor operator
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Tutorial 8/Tutorial 8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 0;
+
+            Console.WriteLine("Enter a number");
+            input = int.Parse(Console.ReadLine());
+
+            if (input >= 0)
+            {
+                Console.WriteLine("This is a positive number");
+            }
+            else
+                Console.WriteLine("you etnered a negative number");
+            Console.ReadLine();
+        }
+    }
+}
