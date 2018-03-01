b3edc9d3e7ca410ac99e91d75422913c56082c3e
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-01 15:52:59 +1300

    tutorial 6 complete

---
+++ Tutorial 6/Tutorial 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pi = Math.PI;
+            var radius = 7.5;
+
+            Console.WriteLine("Enter Radius of Circle: ");
+            radius = double.Parse(Console.ReadLine());
+
+            var power = 2;
+            var rsq = Math.Pow(radius, power);
+
+            Console.WriteLine($"The Perimeter o fthe circle is: {2*pi*radius}");
+            Console.WriteLine($"The Area of the circle is: {pi*rsq}");
+            Console.ReadLine();
+
+        }
+    }
+}
