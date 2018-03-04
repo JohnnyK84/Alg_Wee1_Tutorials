813fa13029d5c838c2e680bc5a82a864e5b56f5f
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-03-04 17:42:46 +1300

    Tutorial 11 complete using node.js

---
+++ Tutorial 11 - Using node.js/rmv_itm_arry.js
@@ -0,0 +1,12 @@
+var array1 = ["beef","Chicken","Rice","Pork","Vego"];
+console.log(array1);
+
+// using splice method to remove item from array, start item 1, remove 1 item
+array1.splice(1,1); 
+console.log(array1);
+
+array1 = ["beef","Chicken","Rice","Pork","Vego"];
+
+// removing more than 1 item, start at position 1 remove 3 tiems
+array1.splice(1,3);
+console.log(array1); 
